3a5ea17d50c966eaefb491f30ac3ad88b719483b
raed.aljamal <raed.aljamal@gizasystems.onmicrosoft.com>
2025-02-03 16:31:41 +0200

    Editing profile username locator

--- pages/header.js
+++ pages/header.js
@@ -2,10 +2,10 @@ const {expect} = require("@playwright/test");
 exports.Header = class PageHeader {
     constructor(page) {
         this.page = page;
-        this.userTitle = page.locator(".user-info span");
+        this.userTitle = page.locator("[class=user-info] span");
     }
 
     async assertOnUserTitle(title) {
-        await expect(await this.userTitle.textContent()).toBe(title)
+        await expect(await this.userTitle).toHaveText(title);
     }
 }
